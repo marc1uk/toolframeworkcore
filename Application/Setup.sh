22b3437daaa34be2e74139212a8d98570c26525b
brichards <benjamin.richards@warwick.ac.uk>
2021-12-21 14:36:53 +0000

    corrected spelling for dependencies

--- Application/Setup.sh
+++ Application/Setup.sh
@@ -2,8 +2,8 @@
 
 #Application path location of applicaiton
 
-Dependancies=`pwd`/Dependancies
+Dependencies=`pwd`/Dependencies
 
-export LD_LIBRARY_PATH=`pwd`/lib:${Dependancies}/ToolFramework/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=`pwd`/lib:${Dependencies}/ToolFramework/lib:$LD_LIBRARY_PATH
 
 export SEGFAULT_SIGNALS="all"
