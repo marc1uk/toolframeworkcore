81fc7dad131055fa8d388eca5f672010ac72427e
Marcus O'Flaherty <moflaherty1@sheffield.ac.uk>
2023-02-24 23:47:42 +0000

    whitespace fixes

--- src/ToolChain/ToolChain.cpp
+++ src/ToolChain/ToolChain.cpp
@@ -1,7 +1,7 @@
 #include "ToolChain.h"
 
 ToolChain::ToolChain(std::string configfile,  int argc, char* argv[]){
- 
+  
   m_data=new DataModel();
   
   if(!m_data->vars.Initialise(configfile)){
@@ -12,7 +12,7 @@ ToolChain::ToolChain(std::string configfile,  int argc, char* argv[]){
   if(!m_data->vars.Get("verbose",m_verbose)) m_verbose=9;
   if(!m_data->vars.Get("error_level",m_errorlevel)) m_errorlevel=2;
   if(!m_data->vars.Get("attempt_recover",m_recover)) m_recover=false;
-
+  
   if(!m_data->vars.Get("log_interactive",m_log_interactive)) m_log_interactive=true;
   if(!m_data->vars.Get("log_local",m_log_local)) m_log_local=false;
   if(!m_data->vars.Get("log_split_files",m_log_split_files)) m_log_split_files=false;
@@ -24,10 +24,10 @@ ToolChain::ToolChain(std::string configfile,  int argc, char* argv[]){
     tmp<<m_log_local_path<<"."<<time(NULL);
     m_log_local_path= tmp.str();
   }
-
+  
   
   if(!m_data->vars.Get("Inline",m_inline)) m_inline=1;
-  if(!m_data->vars.Get("Interactive",m_interactive)) m_interactive=false;  
+  if(!m_data->vars.Get("Interactive",m_interactive)) m_interactive=false;
   
   m_data->vars.Set("argc",argc);
    
@@ -37,20 +37,20 @@ ToolChain::ToolChain(std::string configfile,  int argc, char* argv[]){
     m_data->vars.Set(tmp.str(),argv[i]);
   }
  
-#ifdef DEBUG  
-  m_data->vars.Print(); 
-#endif   
+#ifdef DEBUG
+  m_data->vars.Print();
+#endif
   
   Init();
-
+  
   std::string toolsfile="";
   m_data->vars.Get("Tools_File",toolsfile);
-
+  
   if(!LoadTools(toolsfile)) exit(1);
-
-  if(m_inline!=0) Inline();                                                                           
-  else if(m_interactive) Interactive();  
- 
+  
+  if(m_inline!=0) Inline();
+  else if(m_interactive) Interactive();
+  
   
 }
 
@@ -58,26 +58,26 @@ ToolChain::ToolChain(int verbose, int errorlevel, bool log_interactive, bool log
   
   m_verbose=verbose;
   m_errorlevel=errorlevel;
- 
-  m_log_interactive=log_interactive; 
+  
+  m_log_interactive=log_interactive;
   m_log_local=log_local;
   m_log_split_files=log_split_files;
   m_log_local_path=log_local_path;
-
+  
   if(in_data_model==0) m_data=new DataModel;
   else m_data=in_data_model;
-
+  
   Init();
-
+  
 }
 
 void ToolChain::Init(){
-
+  
   m_log=0;
- 
+  
   m_log= new Logging(m_log_interactive, m_log_local, m_log_local_path, m_log_split_files);
-
-  if(!m_data->Log) m_data->Log=m_log;  
+  
+  if(!m_data->Log) m_data->Log=m_log;
   
   execounter=0;
   Initialised=false;
@@ -85,7 +85,7 @@ void ToolChain::Init(){
   paused=false;
   
   *m_log<<MsgL(1,m_verbose)<<yellow<<"********************************************************\n"<<"**** Tool chain created ****\n"<<"********************************************************\n"<<std::endl;
-
+  
 }
 
 
@@ -99,12 +99,12 @@ bool ToolChain::Add(std::string name,Tool *tool,std::string configfile){
     m_tools.push_back(tool);
     m_toolnames.push_back(name);
     m_configfiles.push_back(configfile);
-
+    
     *m_log<<MsgL(1,m_verbose)<<green<<"Tool='"<<name<<"' added successfully\n"<<std::endl;
-    return true;   
+    return true;
   }
   else{
-    *m_log<<MsgL(0,m_verbose)<<red<<"ERROR!!! Tool='"<<name<<"' Does Not Exist in factory!!! \n"<<std::endl;  
+    *m_log<<MsgL(0,m_verbose)<<red<<"ERROR!!! Tool='"<<name<<"' Does Not Exist in factory!!! \n"<<std::endl;
     return false;
   }
   
@@ -113,54 +113,54 @@ bool ToolChain::Add(std::string name,Tool *tool,std::string configfile){
 
 
 int ToolChain::Initialise(){
-
+  
   bool result=0;
-
+  
   if (Finalised){
     *m_log<<MsgL(1,m_verbose)<<yellow<<"********************************************************\n"<<"**** Initialising tools in toolchain ****\n"<<"********************************************************\n"<<std::endl;
-
-  
-    for(unsigned int i=0 ; i<m_tools.size();i++){  
+    
+    
+    for(unsigned int i=0 ; i<m_tools.size();i++){
       *m_log<<MsgL(2,m_verbose)<<cyan<<"Initialising "<<m_toolnames.at(i)<<std::endl;
       *m_log<<MsgL(0,0);
-
-#ifndef DEBUG      
-      try{ 
-#endif   
-
-	if(m_tools.at(i)->Initialise(m_configfiles.at(i), *m_data))  *m_log<<MsgL(2,m_verbose)<<green<<m_toolnames.at(i)<<" initialised successfully\n"<<std::endl;
-	else{
-	  *m_log<<MsgL(0,m_verbose)<<red<<"WARNING !!!!! "<<m_toolnames.at(i)<<" Failed to initialise (exit error code)\n"<<std::endl;
-	  result=1;
-	  if(m_errorlevel>1) exit(1);
-	}
-	
-#ifndef DEBUG	
-      }      
+      
+#ifndef DEBUG
+      try{
+#endif
+        
+        if(m_tools.at(i)->Initialise(m_configfiles.at(i), *m_data))  *m_log<<MsgL(2,m_verbose)<<green<<m_toolnames.at(i)<<" initialised successfully\n"<<std::endl;
+        else{
+          *m_log<<MsgL(0,m_verbose)<<red<<"WARNING !!!!! "<<m_toolnames.at(i)<<" Failed to initialise (exit error code)\n"<<std::endl;
+          result=1;
+          if(m_errorlevel>1) exit(1);
+        }
+        
+#ifndef DEBUG
+      }
       catch(std::exception& e){
         *m_log<<MsgL(0,m_verbose)<<red<<e.what()<<"\n"<<std::endl;
         throw;
       }
       catch(...){
-	*m_log<<MsgL(0,m_verbose)<<red<<"WARNING !!!!! "<<m_toolnames.at(i)<<" Failed to initialise (uncaught error)\n"<<std::endl;
-	result=2;
-	if(m_errorlevel>0) exit(1);
+        *m_log<<MsgL(0,m_verbose)<<red<<"WARNING !!!!! "<<m_toolnames.at(i)<<" Failed to initialise (uncaught error)\n"<<std::endl;
+        result=2;
+        if(m_errorlevel>0) exit(1);
        }
 #endif
      
     }
-
+    
     *m_log<<MsgL(1,m_verbose)<<yellow<<"**** Tool chain initialised ****\n"<<"********************************************************\n"<<std::endl;
-
+    
     execounter=0;
     Initialised=true;
     Finalised=false;
     }
   else {
-
+    
     *m_log<<MsgL(1,m_verbose)<<purple<<"********************************************************\n"<<" ERROR: ToolChain Cannot Be Initialised as already running. Finalise old chain first \n"<<"********************************************************\n"<<std::endl;
     
-
+    
     result=-1;
   }
   
@@ -170,72 +170,72 @@ int ToolChain::Initialise(){
 
 
 int ToolChain::Execute(int repeates){
- 
+  
   int result =0;
   
   if(Initialised){
     bool skip=false;
     m_data->vars.Set("Skip",skip);
-
+    
     if(m_inline)  *m_log<<MsgL(2,m_verbose)<<yellow<<"********************************************************\n"<<"**** Executing toolchain "<<repeates<<" times ****\n"<<"********************************************************\n"<<std::endl;
-
+    
     for(int i=0;i<repeates;i++){
      
       *m_log<<MsgL(3,m_verbose)<<yellow<<"********************************************************\n"<<"**** Executing tools in toolchain ****\n"<<"********************************************************\n"<<std::endl;
       
       for(unsigned int i=0 ; i<m_tools.size();i++){
-	m_data->vars.Get("Skip",skip); 
-	if(skip){
-	  skip=false;
-	  m_data->vars.Set("Skip",skip);
-	  *m_log<<MsgL(4,m_verbose)<<cyan<<"Skipping Remaining Tools"<<std::endl;
-	  break;
-	}
-
-	*m_log<<MsgL(4,m_verbose)<<cyan<<"Executing "<<m_toolnames.at(i)<<std::endl;
-	*m_log<<MsgL(0,0);
-	
+        m_data->vars.Get("Skip",skip);
+        if(skip){
+          skip=false;
+          m_data->vars.Set("Skip",skip);
+          *m_log<<MsgL(4,m_verbose)<<cyan<<"Skipping Remaining Tools"<<std::endl;
+          break;
+        }
+        
+        *m_log<<MsgL(4,m_verbose)<<cyan<<"Executing "<<m_toolnames.at(i)<<std::endl;
+        *m_log<<MsgL(0,0);
+        
 #ifndef DEBUG
-	try{
+        try{
 #endif
-	  
-	  if(m_tools.at(i)->Execute()) *m_log<<MsgL(4,m_verbose)<<green<<m_toolnames.at(i)<<" executed successfully\n"<<std::endl;
-	   
-	  else{
-	    *m_log<<MsgL(0,m_verbose)<<red<<"WARNING !!!!!! "<<m_toolnames.at(i)<<" Failed to execute (error code)\n"<<std::endl;	    
-	    result=1;
-	    if(m_errorlevel>1){
-	      if(m_recover){
-		m_errorlevel=0;
-		Finalise();
-	      }
-	      exit(1);
-	    }
-	  }  
-	  
+          
+          if(m_tools.at(i)->Execute()) *m_log<<MsgL(4,m_verbose)<<green<<m_toolnames.at(i)<<" executed successfully\n"<<std::endl;
+           
+          else{
+            *m_log<<MsgL(0,m_verbose)<<red<<"WARNING !!!!!! "<<m_toolnames.at(i)<<" Failed to execute (error code)\n"<<std::endl;
+            result=1;
+            if(m_errorlevel>1){
+              if(m_recover){
+                m_errorlevel=0;
+                Finalise();
+              }
+              exit(1);
+            }
+          }
+          
 #ifndef DEBUG
-	}
-	
-	catch(std::exception& e){
-	  *m_log<<MsgL(0,m_verbose)<<red<<e.what()<<"\n"<<std::endl;
-	  throw;
-	}
-	catch(...){
-	  *m_log<<MsgL(0,m_verbose)<<red<<"WARNING !!!!!! "<<m_toolnames.at(i)<<" Failed to execute (uncaught error)\n"<<std::endl;
-	  
-	  result=2;
-	  if(m_errorlevel>0){
-	    if(m_recover){
-	      m_errorlevel=0;
-	      Finalise();
-	    }
-	    exit(1);
-	  }
-	}
-#endif	
-	
+        }
+        
+        catch(std::exception& e){
+          *m_log<<MsgL(0,m_verbose)<<red<<e.what()<<"\n"<<std::endl;
+          throw;
+        }
+        catch(...){
+          *m_log<<MsgL(0,m_verbose)<<red<<"WARNING !!!!!! "<<m_toolnames.at(i)<<" Failed to execute (uncaught error)\n"<<std::endl;
+          
+          result=2;
+          if(m_errorlevel>0){
+            if(m_recover){
+              m_errorlevel=0;
+              Finalise();
+            }
+            exit(1);
+          }
+        }
+#endif
+        
       }
-
+      
       *m_log<<MsgL(3,m_verbose)<<yellow<<"**** Tool chain executed ****\n"<<"********************************************************\n"<<std::endl;
     }
     
@@ -248,51 +248,51 @@ int ToolChain::Execute(int repeates){
     *m_log<<MsgL(0,m_verbose)<<purple<<"********************************************************\n"<<" ERROR: ToolChain Cannot Be Executed As Has Not Been Initialised yet.\n"<<"********************************************************\n"<<std::endl;
     result=-1;
   }
-
+  
   return result;
 }
 
 
 
 int ToolChain::Finalise(){
- 
+  
   int result=0;
   if(Initialised){
     *m_log<<MsgL(1,m_verbose)<<yellow<<"********************************************************\n"<<"**** Finalising tools in toolchain ****\n"<<"********************************************************\n"<<std::endl;
-
+    
     for(unsigned int i=0 ; i<m_tools.size();i++){
       *m_log<<MsgL(2,m_verbose)<<cyan<<"Finalising "<<m_toolnames.at(i)<<std::endl;
       *m_log<<MsgL(0,0);
-
+      
 #ifndef DEBUG
       try{
 #endif
-
-	if(m_tools.at(i)->Finalise()) *m_log<<MsgL(2,m_verbose)<<green<<m_toolnames.at(i)<<" Finalised successfully\n"<<std::endl;
-	
-	else{
-	  *m_log<<MsgL(0,m_verbose)<<red<<"WARNING !!!!!!! "<<m_toolnames.at(i)<<" Finalised successfully (error code)\n"<<std::endl;	  
-	  result=1;
-	  if(m_errorlevel>1)exit(1);
-	}  
-
-#ifndef DEBUG      
+        
+        if(m_tools.at(i)->Finalise()) *m_log<<MsgL(2,m_verbose)<<green<<m_toolnames.at(i)<<" Finalised successfully\n"<<std::endl;
+        
+        else{
+          *m_log<<MsgL(0,m_verbose)<<red<<"WARNING !!!!!!! "<<m_toolnames.at(i)<<" Finalised successfully (error code)\n"<<std::endl;
+          result=1;
+          if(m_errorlevel>1)exit(1);
+        }
+        
+#ifndef DEBUG
       }
       
       catch(std::exception& e){
-	*m_log<<MsgL(0,m_verbose)<<red<<e.what()<<"\n"<<std::endl;
-	throw;
+        *m_log<<MsgL(0,m_verbose)<<red<<e.what()<<"\n"<<std::endl;
+        throw;
       }
       catch(...){
-	*m_log<<MsgL(0,m_verbose)<<red<<"WARNING !!!!!!! "<<m_toolnames.at(i)<<" Finalised successfully (uncaught error)\n"<<std::endl;
-	
-	result=2;
-	if(m_errorlevel>0)exit(1);
+        *m_log<<MsgL(0,m_verbose)<<red<<"WARNING !!!!!!! "<<m_toolnames.at(i)<<" Finalised successfully (uncaught error)\n"<<std::endl;
+        
+        result=2;
+        if(m_errorlevel>0)exit(1);
       }
 #endif
-
+      
     }
-
+    
     *m_log<<MsgL(1,m_verbose)<<yellow<<"**** Toolchain Finalised ****\n"<<"********************************************************\n"<<std::endl;
     
     execounter=0;
@@ -301,20 +301,20 @@ int ToolChain::Finalise(){
     paused=false;
   }
   
-  else { 
+  else {
     *m_log<<MsgL(0,m_verbose)<<purple<<"********************************************************\n"<<std::endl<<" ERROR: ToolChain Cannot Be Finalised As Has Not Been Initialised yet."<<std::endl<<"********************************************************"<<std::endl;
     
     result=-1;
   }
-
-
-
+  
+  
+  
   return result;
 }
 
 
 bool ToolChain::LoadTools(std::string filename){
-
+  
   if(filename!=""){
     std::ifstream file(filename.c_str());
     std::string line;
@@ -323,17 +323,17 @@ bool ToolChain::LoadTools(std::string filename){
       while (getline(file,line)){
         if (line.size()>0){
           if (line.at(0)=='#')continue;
-	  std::string name;
-	  std::string tool;
-	  std::string conf;
-	  std::stringstream stream(line);
-	  
+          std::string name;
+          std::string tool;
+          std::string conf;
+          std::stringstream stream(line);
+          
           if(stream>>name>>tool>>conf){
-	    if(!Add(name,Factory(tool),conf)) return false;
-	  } 
-	} 
+            if(!Add(name,Factory(tool),conf)) return false;
+          }
+        }
       }
-      file.close();        
+      file.close();
       return true;
     }
     else{
@@ -347,11 +347,11 @@ bool ToolChain::LoadTools(std::string filename){
     
     return false;
   }
-
+  
 }
 
 void ToolChain::Inline(){
-
+  
   if(m_inline==-1){
     bool StopLoop=false;
     m_data->vars.Set("StopLoop",StopLoop);
@@ -369,8 +369,8 @@ void ToolChain::Inline(){
     Execute(m_inline);
     Finalise();
   }
-
-
+  
+  
 }
 
 
@@ -378,29 +378,29 @@ void ToolChain::Inline(){
 
 void ToolChain::Interactive(){
   exeloop=false;
-   
+  
   ToolChainargs* args=new ToolChainargs;
-
+  
   args->msgflag=&msgflag;
   args->command="";
-
+  
   pthread_create (&thread[0], NULL, ToolChain::InteractiveThread, args);
-
-  bool running=true; 
+  
+  bool running=true;
   
   msgflag=false;
- 
+  
   while (running){
     
-    if(msgflag){  
-
+    if(msgflag){
+      
       printf("%s \n\n",(ExecuteCommand(args->command)).c_str());
       if(args->command=="Quit"){
-	running=false;
+        running=false;
       }
       
       args->command="";
-
+      
       std::stringstream tmp;
       tmp<<"Please type command : "<<cyan<<"Start, Pause, Unpause, Stop, Status, Quit, ?, (Initialise, Execute, Finalise)"<<plain;
       printf("%s \n %s",tmp.str().c_str(),">");
@@ -416,7 +416,7 @@ void ToolChain::Interactive(){
   delete args;
   args=0;
   
-}  
+}
 
 
 
@@ -457,9 +457,9 @@ std::string ToolChain::ExecuteCommand(std::string command){
     }
     else if(command=="Unpause"){
       if(Initialised){
-	exeloop=true;
-	paused=false;
-	returnmsg<<"Unpausing ToolChain";
+        exeloop=true;
+        paused=false;
+        returnmsg<<"Unpausing ToolChain";
       }
       else returnmsg<<red<<"Error Code "<<Execute()<<plain;
     }
@@ -481,8 +481,8 @@ std::string ToolChain::ExecuteCommand(std::string command){
       if(Finalised) tmp<<"Waiting to Initialise ToolChain";
       if(Initialised && execounter==0) tmp<<"Initialised waiting to Execute ToolChain";
       if(Initialised && execounter>0){
-	if(paused)tmp<<"ToolChain execution pasued";
-	else tmp<<"ToolChain running (loop counter="<<execounter<<")";
+        if(paused)tmp<<"ToolChain execution pasued";
+        else tmp<<"ToolChain running (loop counter="<<execounter<<")";
       }
       
       if(*(m_data->vars["Status"])!="") tmp<<" : "<<*(m_data->vars["Status"]);
@@ -500,13 +500,13 @@ std::string ToolChain::ExecuteCommand(std::string command){
 
 
 void* ToolChain::InteractiveThread(void* arg){
-
+  
   ToolChainargs* args=static_cast<ToolChainargs*>(arg);
-
+  
   bool running=true;
   
   printf("%s %s %s %s\n %s %s %s","Please type command :",cyan," Start, Pause, Unpause, Stop, Restart, Status, Quit, ?, (Initialise, Execute, Finalise)",plain,green,">",plain);
-
+  
   while (running){
     
     std::cin>>args->command;
@@ -520,7 +520,7 @@ void* ToolChain::InteractiveThread(void* arg){
 
 
 ToolChain::~ToolChain(){
- 
+  
   for (unsigned int i=0;i<m_tools.size();i++){
     delete m_tools.at(i);
     m_tools.at(i)=0;
@@ -529,12 +529,12 @@ ToolChain::~ToolChain(){
   m_tools.clear();
   
   if(m_data!=0 && m_data->Log==m_log){
-    m_data->Log=0; 
+    m_data->Log=0;
     delete m_data;
     m_data=0;
   }
-
-  delete m_log;  
+  
+  delete m_log;
   m_log=0;
   
 }
